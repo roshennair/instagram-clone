4eadfec297901a2379260b440416310e2c675b47
Roshen Nair <roshen620@gmail.com>
2021-09-05 12:17:32 +0800

    Implement image selection from device camera and image gallery

--- README.md
+++ README.md
@@ -3,3 +3,11 @@
 An Instagram-like photo sharing mobile application built using React Native, Expo, Google Firebase, and Redux.
 
 **DISCLAIMER**: I did not design/build this project entirely by myself, although I have made some modifications and improvements over the original design. I created it as a code-along project for SimCoder's excellent tutorial, [Build an Instagram Clone with React Native, Firebase Firestore, Redux, Expo - Full Course](https://www.youtube.com/watch?v=1hPgQWbWmEk).
+
+## What I learned:
+
+- Created a new React Native project using Expo.
+- Implemented basic in-app navigation using StackNavigator.
+- Implemented email-password authentication using Firebase Authentication.
+- Used Redux for fetching and managing app state.
+- Implemented bottom tab navigation using TabNavigator.
--- components/main/Add.js
+++ components/main/Add.js
@@ -1,12 +1,89 @@
-import React, { Component } from 'react'
-import { Text, View } from 'react-native'
+import React, { useState, useEffect } from 'react';
+import { Text, View, Button, StyleSheet, Image } from 'react-native';
+import { Camera } from 'expo-camera';
+import * as ImagePicker from 'expo-image-picker';
 
-export default class Add extends Component {
-	render() {
-		return (
-			<View>
-				<Text>Add</Text>
-			</View>
-		)
+export default function App() {
+	const [hasCameraPermission, setHasCameraPermission] = useState(null);
+	const [hasGalleryPermission, setHasGalleryPermission] = useState(null);
+	const [type, setType] = useState(Camera.Constants.Type.back);
+	const [camera, setCamera] = useState(null);
+	const [image, setImage] = useState(null);
+
+	useEffect(() => {
+		(async () => {
+			const { status: cameraStatus } = await Camera.requestPermissionsAsync();
+			setHasCameraPermission(cameraStatus === 'granted');
+
+			const { status: galleryStatus } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+			setHasGalleryPermission(galleryStatus === 'granted');
+		})();
+	}, []);
+
+	const takePicture = async () => {
+		if (camera) {
+			let photo = await camera.takePictureAsync(null);
+			setImage(photo.uri);
+		}
 	}
+
+	const pickImage = async () => {
+		let result = await ImagePicker.launchImageLibraryAsync({
+			mediaTypes: ImagePicker.MediaTypeOptions.Images,
+			allowsEditing: true,
+			aspect: [1, 1],
+			quality: 1,
+		});
+
+		if (!result.cancelled) {
+			setImage(result.uri);
+		}
+	};
+
+	if (hasCameraPermission === null || hasGalleryPermission === null) {
+		return <View />;
+	}
+	if (hasCameraPermission === false || hasGalleryPermission === false) {
+		return <Text>No access to camera</Text>;
+	}
+	return (
+		<View style={{ flex: 1 }}>
+			<View style={styles.cameraContainer}>
+				<Camera
+					ref={el => setCamera(el)}
+					type={type}
+					style={styles.fixedRatio}
+					ratio="1:1" />
+			</View>
+			<Button
+				title="Flip Camera"
+				onPress={() => {
+					setType(
+						type === Camera.Constants.Type.back
+							? Camera.Constants.Type.front
+							: Camera.Constants.Type.back
+					);
+				}} />
+			<Button
+				title="Take Picture"
+				onPress={takePicture} />
+			<Button
+				title="Pick Image from Gallery"
+				onPress={pickImage} />
+			{image && <Image
+				source={{ uri: image }}
+				style={{ flex: 1 }} />}
+		</View>
+	);
 }
+
+const styles = StyleSheet.create({
+	cameraContainer: {
+		flex: 1,
+		flexDirection: 'row'
+	},
+	fixedRatio: {
+		flex: 1,
+		aspectRatio: 1
+	}
+});
